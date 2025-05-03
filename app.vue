57cea7f6bbcf958292f91f286fc2f5ff51efabf6
Patryk Kudlik <patryk.kudlik@gmail.com>
2025-05-03 10:41:57 +0200

    refactor(favicon assets): remove old SVG files and replace with PNGs; update manifest and HTML head for new favicon paths and sizes

--- app.vue
+++ app.vue
@@ -70,22 +70,129 @@ useHead({
     link: [
         {
             rel: 'shortcut icon',
-            href: '/favicons/nuxt3-favicon.svg',
-            type: 'image/svg+xml'
+            href: '/favicons/favicon.ico'
         },
         {
-            rel: 'icon',
-            href: '/favicons/nuxt3-favicon.svg',
-            type: 'image/svg+xml'
+            rel: 'apple-touch-icon',
+            sizes: '57x57',
+            href: '/favicons/apple-icon-57x57.png'
+        },
+        {
+            rel: 'apple-touch-icon',
+            sizes: '60x60',
+            href: '/favicons/apple-icon-60x60.png'
+        },
+        {
+            rel: 'apple-touch-icon',
+            sizes: '72x72',
+            href: '/favicons/apple-icon-72x72.png'
+        },
+        {
+            rel: 'apple-touch-icon',
+            sizes: '76x76',
+            href: '/favicons/apple-icon-76x76.png'
+        },
+        {
+            rel: 'apple-touch-icon',
+            sizes: '114x114',
+            href: '/favicons/apple-icon-114x114.png'
+        },
+        {
+            rel: 'apple-touch-icon',
+            sizes: '120x120',
+            href: '/favicons/apple-icon-120x120.png'
+        },
+        {
+            rel: 'apple-touch-icon',
+            sizes: '144x144',
+            href: '/favicons/apple-icon-144x144.png'
+        },
+        {
+            rel: 'apple-touch-icon',
+            sizes: '152x152',
+            href: '/favicons/apple-icon-152x152.png'
         },
         {
             rel: 'apple-touch-icon',
             sizes: '180x180',
-            href: '/favicons/nuxt3-apple-touch.svg'
+            href: '/favicons/apple-icon-180x180.png'
+        },
+        {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '512x512',
+            href: '/favicons/favicon-512x512.png'
+        },
+        {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '192x192',
+            href: '/favicons/android-icon-192x192.png'
+        },
+        {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '144x144',
+            href: '/favicons/favicon-144x144.png'
+        },
+        {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '96x96',
+            href: '/favicons/favicon-96x96.png'
+        },
+        {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '72x72',
+            href: '/favicons/favicon-72x72.png'
+        },
+        {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '48x48',
+            href: '/favicons/favicon-48x48.png'
+        },
+        {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '36x36',
+            href: '/favicons/favicon-36x36.png'
+        },
+        {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '32x32',
+            href: '/favicons/favicon-32x32.png'
+        },
+        {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '16x16',
+            href: '/favicons/favicon-16x16.png'
         },
         {
             rel: 'manifest',
             href: '/manifest.json'
+        }
+    ],
+
+    meta: [
+        {
+            name: 'msapplication-config',
+            content: 'browserconfig.xml'
+        },
+        {
+            name: 'msapplication-TileImage',
+            content: '/favicons/ms-icon-144x144.png'
+        },
+        {
+            name: 'msapplication-TileColor',
+            content: '#ffffff'
+        },
+        {
+            name: 'theme-color',
+            content: '#ffffff'
         }
     ]
 });
