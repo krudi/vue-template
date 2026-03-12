0b957eef437756620bb16ba355bf0bef5b145eaf
Patryk Kudlik <patryk.kudlik@gmail.com>
2026-03-12 17:14:30 +0100

    chore(eslint): allow self-closing void elements in Vue templates

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,18 @@ import withNuxt from './.nuxt/eslint.config.mjs';
 export default withNuxt({
     files: ['**/*.vue'],
     rules: {
+        'vue/html-self-closing': [
+            'error',
+            {
+                html: {
+                    void: 'always',
+                    normal: 'never',
+                    component: 'always',
+                },
+                svg: 'always',
+                math: 'always',
+            },
+        ],
         'vue/multi-word-component-names': 'off',
     },
 });
